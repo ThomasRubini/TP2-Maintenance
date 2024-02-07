a99c83483494e6ca8e1cebc24572932b7fec970a
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-02-07 11:23:11 +0100

    add q10

---
+++ src/main/java/org/example/q10/Regex.java
@@ -0,0 +1,20 @@
+package org.example.q10;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Regex
+{
+    private static final Pattern p = Pattern.compile("\\d{3}");
+    public static String[] threeDigitNumbers(String source)
+    {
+        ArrayList<String> retVal = new ArrayList<String>();
+        Matcher m = p.matcher(source);
+        while (m.find())
+        {
+            retVal.add(m.group());
+        }
+        String[] retValArray = new String[retVal.size()];
+        return retVal.toArray(retValArray);
+    }
+}
---
+++ src/test/java/org/example/q10/RegexTest.java
@@ -0,0 +1,39 @@
+package org.example.q10;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegexTest {
+    @Test
+    public void basicTest() {
+        assertArrayEquals(new String[]{"012"}, Regex.threeDigitNumbers("012"));
+    }
+    @Test
+    public void fourDigits() {
+        assertArrayEquals(new String[]{"567"}, Regex.threeDigitNumbers("5678"));
+    }
+    @Test
+    public void multipleMatches() {
+        assertArrayEquals(new String[]{"567", "987"}, Regex.threeDigitNumbers("567a987"));
+    }
+    @Test
+    public void multipleMatches2() {
+        assertArrayEquals(new String[]{"567", "987"}, Regex.threeDigitNumbers("567987"));
+    }    @Test
+    public void sameMatch() {
+        assertArrayEquals(new String[]{"567", "567"}, Regex.threeDigitNumbers("567567"));
+    }
+    @Test
+    public void noMatch() {
+        assertArrayEquals(new String[]{}, Regex.threeDigitNumbers(""));
+        assertArrayEquals(new String[]{}, Regex.threeDigitNumbers("01"));
+        assertArrayEquals(new String[]{}, Regex.threeDigitNumbers("hello"));
+    }
+}
