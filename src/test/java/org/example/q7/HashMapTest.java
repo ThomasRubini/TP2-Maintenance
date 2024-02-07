11e1e1fafa3bd9f2d4765171c664cade4d182e83
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-02-07 10:52:49 +0100

    "optimise" q7

--- src/test/java/org/example/q7/HashMapTest.java
+++ src/test/java/org/example/q7/HashMapTest.java
@@ -1,5 +1,6 @@
 package org.example.q7;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -9,9 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class HashMapTest {
 
+    HashMap<String, String> map;
+
+    @BeforeEach
+    public void init() {
+        map = new HashMap<>();
+    }
+
     @Test
     public void testPut() {
-        HashMap<String, String> map = new HashMap<>();
         map.put("a", "b");
 
         assertEquals("b", map.get("a"));
@@ -19,7 +26,6 @@ public class HashMapTest {
 
     @Test
     public void testReplace() {
-        HashMap<String, String> map = new HashMap<>();
         map.put("a", "b");
         map.put("a", "c");
 
@@ -28,7 +34,6 @@ public class HashMapTest {
 
     @Test
     public void testClear() {
-        HashMap<String, String> map = new HashMap<>();
         map.put("a", "b");
         map.put("c", "d");
         map.clear();
@@ -39,7 +44,6 @@ public class HashMapTest {
 
     @Test
     public void testNullKey() {
-        HashMap<String, String> map = new HashMap<>();
         map.put(null, "b");
 
         assertEquals("b", map.get(null));
